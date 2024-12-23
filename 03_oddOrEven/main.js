b0fb7a4db795ae615facd5c5910dbc6d575e8a79
Gabriel Nascimento <123754383+GabrielNP96@users.noreply.github.com>
2024-12-23 19:05:31 +0000

    par ou ímpar

---
+++ 03_oddOrEven/main.js
@@ -0,0 +1,17 @@
+function oddOrEven(number) {
+
+    if(typeof(number) !== 'number') {
+        return 'Digite apenas números...'
+    }
+
+    if(number % 2 == 0) {
+        return `${number} é par.`
+    } else {
+        return `${number} é ímpar.`
+    }
+}
+
+console.log(oddOrEven(4))
+console.log(oddOrEven(1))
+console.log(oddOrEven(0))
+console.log(oddOrEven('1'))
