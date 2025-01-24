5d371742e42de3484cb80e6a18386e3e884e8f73
Gabriel Nascimento <123754383+GabrielNP96@users.noreply.github.com>
2025-01-24 12:36:37 +0000

    Nova atividade

---
+++ 06_encontrarElementosUnicosEmComum/index.js
@@ -0,0 +1,6 @@
+/*
+Crie uma função que receba duas listas de números como parâmetro, 
+e retorne uma nova lista contendo os elementos que estão presentes em ambas as listas, mas apenas uma vez. Ou seja, 
+se um elemento estiver presente nas duas listas, ele deve aparecer apenas uma vez na lista resultante.
+*/
+
