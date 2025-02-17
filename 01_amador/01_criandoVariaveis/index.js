cd42ba86280818a79ad17c13656e618f0348f154
Gabriel Nascimento <123754383+GabrielNP96@users.noreply.github.com>
2025-02-17 16:59:52 +0000

    ativida sobre variaveis

---
+++ 01_amador/01_criandoVariaveis/index.js
@@ -0,0 +1,7 @@
+//Crie três variáveis para armazenar um nome, idade e cidade onde mora. Exiba essas informações em um único texto concatenado.
+
+const userName = "Rozivaldo";
+const userAge = '44';
+const userAddress = 'middle-earth';
+
+console.log(`Nome: ${userName}\n$idade: ${userAge}\n${userAddress}`);
